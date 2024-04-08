f509054fd65ebd5a3e8030eb9e890be7b3dc4a84
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-08 18:54:05 +0700

    add: test 1

---
+++ tests/1.js
@@ -0,0 +1,14 @@
+const inputDataSets = [
+    "5\n3\n",
+    "10\n5\n",
+    "100\n200\n",
+]
+
+const expectedOutputs = [
+    "8",
+    "15",
+    "300",
+]
+
+exports.inputDataSets = inputDataSets;
+exports.expectedOutputs = expectedOutputs;
