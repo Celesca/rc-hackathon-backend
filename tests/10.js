b74bbe8a12d3516c4be6acb21fe4aa6b35a570c5
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-13 17:23:17 +0700

    ✅ add: test No.10-11

---
+++ tests/10.js
@@ -0,0 +1,19 @@
+// 10. Hard - Bacteria
+
+const inputDataSets = [
+    "5\n",
+    "16\n",
+    "13\n",
+    "175\n",
+    "0\n",
+]
+
+const expectedOutputs = [
+    "2\n",
+    "1\n",
+    "3\n",
+    "6\n",
+    "0\n",
+]
+
+module.exports = { inputDataSets, expectedOutputs }
