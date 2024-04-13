b733b40afa7b0c97d1010693b3c92afdb0a726c4
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-13 17:06:49 +0700

    removed: CI/CD

---
+++ tests/6.js
@@ -0,0 +1,19 @@
+// 6. Medium - Password
+
+const inputDataSets = [
+    "0ACFBD\n",
+    "#EEBGDBA\n",
+    "4BBAGDFFC\n",
+    "1HGBCAE\n",
+    "6GECBBDGH\n",
+]
+
+const expectedOutputs = [
+    "0*7569\n",
+    "#8457*0*\n",
+    "45657*863\n",
+    "1HGBCAE\n",
+    "68412368#\n",
+]
+
+module.exports = { inputDataSets, expectedOutputs }
