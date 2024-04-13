fbdcf3b9134df1216f49d4ba7827ecbbea3a3be2
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-13 17:00:48 +0700

    ✅ add: test No.4

--- tests/4.js
+++ tests/4.js
@@ -2,10 +2,17 @@
 
 const inputDataSets = [
     "3\n0\n",
+    "4\n2\n",
+    "5\n4\n",
+    "2\n1\n",
 ]
 
 const expectedOutputs = [
     "\\       /\n  \\     /\n   \\---/\n",
+    "\\         /\n\\       /\n\\^^^^^/\n\\---/",
+    "\\           /\n\\^^^^^^^^^/\n\\       /\n\\     /\n\\---/",
+    "\\     /\n\\---/",
+
 ]
 
 module.exports = { inputDataSets, expectedOutputs }
